ee9986ca7b218e41e061a8ac5ad68613f3fb7607
Henry Omino <henryedwin92@gmail.com>
2025-09-07 21:57:32 +0300

    tailwind configuration

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // looks inside src for class usage
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
